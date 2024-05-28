983290a9afc837d6fdbb563a38b9a164c63b80cf
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-28 17:27:41 +0900

    chore: 앱라우터 주석

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -15,19 +15,30 @@ import PrivateRoute from "./PrivateRoute";
 const AppRouter = () => {
     return (
         <Routes>
+            {/* 홈 페이지 */}
             <Route path="/" element={<ProductAll />} />
+            
+            {/* 로그인 페이지 */}
             <Route path="/login" element={<Login />} />
+            
+            {/* 회원가입 페이지 */}
             <Route path="/register" element={<RegisterPage />} />
+            
+            {/* 제품 상세 페이지 */}
             <Route path="/product/:id" element={<ProductDetail />} />
+            
+            {/* 고객 권한이 필요한 페이지 */}
             <Route element={<PrivateRoute permissionLevel="customer" />}>
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/payment" element={<PaymentPage />} />
-                <Route path="/payment/success" element={<OrderCompletePage />} />
-                <Route path="/account/purchase" element={<MyPage />} />
+                <Route path="/cart" element={<CartPage />} /> {/* 장바구니 페이지 */}
+                <Route path="/payment" element={<PaymentPage />} /> {/* 결제 페이지 */}
+                <Route path="/payment/success" element={<OrderCompletePage />} /> {/* 주문 완료 페이지 */}
+                <Route path="/account/purchase" element={<MyPage />} /> {/* 내 주문 페이지 */}
             </Route>
+            
+            {/* 관리자 권한이 필요한 페이지 */}
             <Route element={<PrivateRoute permissionLevel="admin" />}>
-                <Route path="/admin/product" element={<AdminProduct />} />
-                <Route path="/admin/order" element={<AdminOrderPage />} />
+                <Route path="/admin/product" element={<AdminProduct />} /> {/* 관리자 제품 관리 페이지 */}
+                <Route path="/admin/order" element={<AdminOrderPage />} /> {/* 관리자 주문 관리 페이지 */}
             </Route>
         </Routes>
     );
