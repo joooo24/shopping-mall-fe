525048a3ba6ad37af69bccb94110c1e7cd8279f1
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-07 16:43:08 +0900

    feat: 카드 클릭 시 상세페이지 이동

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -5,16 +5,15 @@ import "./../style/product.style.scss";
 
 const ProductCard = ({ productList }) => {
     const navigate = useNavigate();
-    console.log("### productList", productList);
-
-    const showProduct = (id) => {
-        // 상품 디테일 페이지로 가기
+    const showProduct = (productId) => {
+        // 상품 상세 페이지로 이동 /product/:id
+        navigate(`/product/${productId}`);
     };
 
     return (
         <>
             {productList?.map((product, index) => (
-                <div key={product._id} className="product-card" onClick={() => showProduct("hard_code")}>
+                <div key={product._id} className="product-card" onClick={() => showProduct(product._id)}>
                     <img src={product.image} alt={product.name} />
                     <div>{product.name}</div>
                     <div>{currencyFormat(product.price)}원</div> {/* currencyFormat 사용하여 가격 형식 변환 */}
