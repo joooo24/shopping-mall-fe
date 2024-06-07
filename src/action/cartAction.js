a035079208b6e1c64804c6aca1f4d928e9384e0a
claire <61533589+joooo24@users.noreply.github.com>
2024-06-07 21:37:56 +0900

    feat: 장바구니 담기 액션 리듀서 작성

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -3,7 +3,24 @@ import * as types from "../constants/cart.constants";
 import { commonUiActions } from "../action/commonUiAction";
 const addToCart =
     ({ id, size }) =>
-    async (dispatch) => {};
+    async (dispatch) => {
+        try {
+            // 장바구니에 아이템 담기 요청
+            dispatch({ type: types.ADD_TO_CART_REQUEST });
+
+            // 서버에 장바구니에 아이템 담기 요청
+            const response = await api.post("/cart", { id, size, qty: 1 });
+            if (response.status !== 200) {
+                throw new Error(response.error);
+            }
+            // console.log("### cart response", response)
+
+            // 장바구니에 아이템 담기 요청 성공
+            dispatch({ type: types.ADD_TO_CART_SUCCESS, payload: response.data });
+        } catch (err) {
+            dispatch({ type: types.ADD_TO_CART_FAIL });
+        }
+    };
 
 const getCartList = () => async (dispatch) => {};
 const deleteCartItem = (id) => async (dispatch) => {};
--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,14 +1,30 @@
 import * as types from "../constants/cart.constants";
-import {
-    LOGIN_SUCCESS,
-    GOOGLE_LOGIN_SUCCESS,
-    LOGOUT,
-} from "../constants/user.constants";
+import { LOGIN_SUCCESS, GOOGLE_LOGIN_SUCCESS, LOGOUT } from "../constants/user.constants";
 
-const initialState = {};
+const initialState = {
+    loading: true, // 로딩 상태
+    user: null, // 사용자 정보
+    error: "", // 에러 메시지
+    cart: null, // 장바구니
+};
 
 function cartReducer(state = initialState, action) {
     const { type, payload } = action;
-    return state;
+    switch (type) {
+        // 장바구니 담기 요청
+        case types.ADD_TO_CART_REQUEST:
+            return { ...state, loading: true };
+
+        // 장바구니 담기 성공
+        case types.ADD_TO_CART_SUCCESS:
+            return { ...state, loading: false, cart: payload.data};
+
+        // 장바구니 담기 실패
+        case types.ADD_TO_CART_FAIL:
+            return { ...state, loading: false, error: payload.message };
+
+        default:
+            return state;
+    }
 }
 export default cartReducer;
