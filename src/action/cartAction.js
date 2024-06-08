67cec42b5da9da1c06aa66ef340bdaea89c46975
claire <61533589+joooo24@users.noreply.github.com>
2024-06-08 14:30:30 +0900

    chore: cartAction 수정

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -9,18 +9,18 @@ const addToCart =
             dispatch({ type: types.ADD_TO_CART_REQUEST });
 
             // 서버에 장바구니에 아이템 담기 요청
-            const response = await api.post("/cart", { id, size, qty: 1 });
+            const response = await api.post("/cart", { productId: id, size, qty: 1 });
             if (response.status !== 200) {
                 throw new Error(response.error);
             }
-            
+
             // 장바구니에 아이템 담기 요청 성공
             dispatch({ type: types.ADD_TO_CART_SUCCESS, payload: response.data });
 
             // 토스트 알림
             dispatch(commonUiActions.showToastMessage("상품을 장바구니에 담았습니다", "success"));
         } catch (err) {
-            dispatch({ type: types.ADD_TO_CART_FAIL });
+            dispatch({ type: types.ADD_TO_CART_FAIL, payload: err.error });
 
             // 토스트 알림
             dispatch(
