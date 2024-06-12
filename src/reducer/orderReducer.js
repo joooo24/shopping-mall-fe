b77d1d23abe40c8070e591420d448cb2d4240894
주현정 <61533589+joooo24@users.noreply.github.com>
2024-06-12 13:19:08 +0900

    chore: 주석

--- src/reducer/orderReducer.js
+++ src/reducer/orderReducer.js
@@ -21,35 +21,45 @@ function orderReducer(state = initialState, action) {
                 ...state,
                 loading: true,
             };
+        // 주문 생성
         case types.CREATE_ORDER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 orderNum: payload
             };
+        // 주문 리스트 가져오기 (사용자)
         case types.GET_ORDER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 orderList: payload.data
-                // myOrder: payload.data
             };
+        // 주문 리스트 가져오기 (관리자)
         case types.GET_ORDER_LIST_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 orderList: payload,
             };
+        // 주문 업데이트
         case types.UPDATE_ORDER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 orderList: payload,
             };
+
+        // 모달: 선택 된 주문
+        case types.SET_SELECTED_ORDER:
+            return {
+                ...state, selectedOrder: payload
+            };
         case types.CREATE_ORDER_FAIL:
         case types.GET_ORDER_FAIL:
         case types.GET_ORDER_LIST_FAIL:
         case types.UPDATE_ORDER_FAIL:
+
             return {
                 ...state,
                 loading: false,
